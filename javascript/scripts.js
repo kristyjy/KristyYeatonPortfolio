2521ff9491fad81f8d4f28d93799cdc40d70e5df
Kristy Yeaton <kristyjy@gmail.com>
2017-04-21 22:08:04 -0400

    Remove buggy parallax code and update copyright date

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,27 +5,7 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 
 		$('.main-menu').toggleClass('open');
-		if($('.main-menu').hasClass('open')){
-			$('.main-menu').slideDown();
-		}
-		else {
-			$('.main-menu').slideUp();
-		}
-	});
-
-});
-
-$(window).scroll(function(){
-
-	// Parallax Scrolling //
-	$('.parallax-container').each(function(){
-		if ($(this).offset().top < $(window).scrollTop()) {
-			var difference = $(window).scrollTop() - $(this).offset().top;
-			var half = (difference / 2) + 'px';
-			$(this).find('img').css('top', half);
-		} else {
-			$(this).find('img').css('top', '0');
-		}
+		$('.main-menu').slideToggle();
 	});
 
 });
